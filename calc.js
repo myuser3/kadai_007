0b2509ba8c5af48887636e32ffd6601cacae7dd5
myuser3 <138294906+myuser3@users.noreply.github.com>
2023-08-09 20:04:04 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,11 +1,18 @@
 let num = Math.floor(Math.random() * 1000);
+//console.log(num);
 
-if((num % 3) === 0) {
+if(num % 3 === 0 && num % 5 !== 0 ) {
   console.log('3の倍数です');
-} else if ((num % 5) === 0) {
+}
+
+else if (num % 3 !== 0 && num %5 ===0 ) {
   console.log('5の倍数です');
-} if ((num % 3) === 0 && (num % 5) === 0 ) {
+} 
+
+if(num % 3 === 0 && num % 5 ===0 ) {
   console.log('3と5の倍数です');
-} else {
+}
+
+else {
   console.log(num);
 }
